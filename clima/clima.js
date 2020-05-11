aa76849e17c40f02634fb950c30d0f146a46d63e
Marcelo <chiche84@hotmail.com>
2020-05-11 14:55:33 -0300

    ya funciona

--- clima/clima.js
+++ clima/clima.js
@@ -5,7 +5,7 @@ const getClima = async(lat, long) => {
     const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${ lat}&lon=${ long}&units=metric&appid=a7559c580e5e1a5165c9eb1807dbd99d`);
 
 
-    return resp.data;
+    return resp.data.main.temp;
 }
 
 module.exports = {
